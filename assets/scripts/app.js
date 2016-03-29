f1168277283795f1b76b665695fa28b5469f70af
bmaupin <bmaupin@users.noreply.github.com>
2016-03-29 12:49:36 -0400

    Set minimum length dynamically

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,51 +5,65 @@ angular.module('app', ['ui.bootstrap', 'angularBootstrapNumberpicker'])
     $scope.includeNumbers = true;
     $scope.includeSymbols = true;
 
-//    $log.log($scope);
-
     $scope.changeTooltip = function() {
       $scope.dynamicTooltip = 'Copied!';
-    }
+    };
 
-    $scope.resetTooltip = function() {
+    var resetTooltip = function() {
       $scope.dynamicTooltip = 'Copy to clipboard';
-    }
-  
+    };
+
     $scope.generateNewPass = function() {
       $scope.newPassword = WeightedPasswordGen.genPassword();
-      $scope.resetTooltip();
+      resetTooltip();
     };
-    
-    $scope.generateNewPass();
-    
+
+    var setMin = function() {
+      var enabledClasses = 0;
+
+      for (var className in WeightedPasswordGen.charClasses) {
+        if (WeightedPasswordGen.charClasses[className].enabled) {
+          enabledClasses ++;
+        }
+      }
+
+      // A length of less than 1 is nonsensical
+      $scope.min = enabledClasses || 1;
+
+      if ($scope.passwordLength < $scope.min) {
+        $scope.value = $scope.min;
+      }
+    };
+
     $scope.updateClasses = function() {
       WeightedPasswordGen.charClasses["lowerLetters"].enabled = $scope.includeLower;
       WeightedPasswordGen.charClasses["upperLetters"].enabled = $scope.includeUpper;
       WeightedPasswordGen.charClasses["numbers"].enabled = $scope.includeNumbers;
       WeightedPasswordGen.charClasses["symbols"].enabled = $scope.includeSymbols;
-    
+
+      setMin();
       $scope.generateNewPass();
     };
 
     $scope.updateLength = function() {
-//      $log.log('Value changed=' + $scope.passwordLength);/*
       WeightedPasswordGen.setPasswordLength($scope.passwordLength);
       $scope.generateNewPass();
-
-//      $log.log('WeightedPasswordGen.getPasswordLength()=' + WeightedPasswordGen.getPasswordLength());
     };
+
+    setMin();
+    $scope.generateNewPass();
 
     // Instantiate clipboard.js (https://clipboardjs.com/)
     new Clipboard('#copyPasswordButton');
   }])
-  
+
   // Directive to select the password text when clicking on it for easier copying
   .directive('selectOnClick', function () {
     return {
       restrict: 'A',
       link: function (scope, element, attrs) {
         element.on('click', function () {
-          var selection = window.getSelection();            
+          var selection = window.getSelection();
           var range = document.createRange();
           range.selectNodeContents(this);
           selection.removeAllRanges();
